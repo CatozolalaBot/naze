c14005b937494aa2e5008b27d648a7c553196b3b
CatozolalaBot <113823437+CatozolalaBot@users.noreply.github.com>
2022-10-02 22:48:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6281938830020']
+global.ownernomer = "6281938830020"
+global.premium = ['6281938830020']
+global.packname = 'Sticker  Catoz'
+global.author = 'Catoz'
 global.sessionName = 'nazedev'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
